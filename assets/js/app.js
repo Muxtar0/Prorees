b0cc3807a32ba594a3266207ae825d6a3df8903d
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-01-15 20:36:18 +0400

    Finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,43 @@ for(let i = 0 ; i < items.length ; i++){
     })
 }
 
+const mobileopenmenu = document.querySelector('.mobileopenmenu');
+const haburgermenuicon = document.querySelector('.haburgermenuicon');
+const header = document.querySelector('.header');
+
+haburgermenuicon.addEventListener('click' , (e) => {
+    mobileopenmenu.classList.toggle('active')
+    haburgermenuicon.classList.toggle('is-active')
+    header.classList.toggle('active')
+})
+
+const filtertarget = document.querySelector('.recomendedproperties .filtertarget')
+const filteritems = document.querySelectorAll('.realfiltercontents .item')
+const targetbtns = document.querySelectorAll('.recomendedproperties .filtertarget .tragetbtn')
+
+filtertarget.addEventListener('click' , (e) => {
+    if(e.target.classList.contains('tragetbtn')){
+        for(let i = 0 ; i < targetbtns.length; i++){
+            targetbtns[i].classList.remove('active')
+        }
+        for(let i = 0 ; i < filteritems.length; i++){
+            filteritems[i].classList.remove('active')
+        }
+        console.log(e.target.getAttribute('data-target'))
+        e.target.classList.add('active')
+        if(e.target.getAttribute('data-target') == 'all'){
+            for(let i = 0 ; i < filteritems.length; i++){
+                filteritems[i].classList.add('active')
+            }
+        }
+        else{
+            for(let i = 0 ; i < filteritems.length ; i++){
+                if( e.target.getAttribute('data-target') === filteritems[i].getAttribute('data-kind')){
+                    console.log(filteritems[i].getAttribute('data-kind'))
+    
+                    filteritems[i].classList.add('active');
+                }
+            }
+        }
+    }
+})
